26593dd37c0e0704684d09433ea02a55113a782b
Yan Felipe Grando <yangrando@gmail.com>
2025-12-26 21:33:16 -0300

    Initial Commit

---
+++ Lumen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lumen
+//
+//  Created by Yan Felipe Grando on 26/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lumen/LumenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenApp.swift
+//  Lumen
+//
+//  Created by Yan Felipe Grando on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LumenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
